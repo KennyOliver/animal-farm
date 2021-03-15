1bc8f2e8cf9f9e6ce06bf86079316715b0b5b8f5
Kenny Oliver <kenny_oliver@icloud.com>
2021-03-15 07:41:49 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var audio = document.createElement('audio');
+var source = document.createElement('source');
+var cow = document.getElementById('cow-sound');
+cow.appendChild(audio);
+audio.appendChild(source);
+source.setAttribute('src', 'http://www.echoswebmagic.com/sounds/moo.wav');
+source.setAttribute('type', 'audio/mpeg');
+audio.setAttribute('controls', 'controls');
